c0ac61f0ab97cdbceb1bce9c349091c2df64cd64
Brian Watling <brianwatling@hotmail.com>
2011-10-17 22:17:09 -0400

    updated mutex tests

--- test/test_pthread_mutex.c
+++ test/test_pthread_mutex.c
@@ -1,9 +1,8 @@
-#include "fiber_mutex.h"
-#include "fiber_manager.h"
+#include "pthread.h"
 #include "test_helper.h"
 
 int volatile counter = 0;
-fiber_mutex_t mutex;
+pthread_mutex_t mutex;
 #define PER_FIBER_COUNT 100000
 #define NUM_FIBERS 100
 #define NUM_THREADS 4
@@ -12,27 +11,27 @@ void* run_function(void* param)
 {
     int i;
     for(i = 0; i < PER_FIBER_COUNT; ++i) {
-        fiber_mutex_lock(&mutex);
+        pthread_mutex_lock(&mutex);
         ++counter;
-        fiber_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
 
 int main()
 {
-    fiber_manager_set_total_kernel_threads(NUM_THREADS);
+    //fiber_manager_set_total_kernel_threads(NUM_THREADS);
 
-    fiber_mutex_init(&mutex);
+    pthread_mutex_init(&mutex, NULL);
 
-    fiber_t* fibers[NUM_FIBERS];
+    pthread_t fibers[NUM_FIBERS];
     int i;
     for(i = 0; i < NUM_FIBERS; ++i) {
-        fibers[i] = fiber_create(20000, &run_function, NULL);
+        pthread_create(&fibers[i], NULL, &run_function, NULL);
     }
 
     for(i = 0; i < NUM_FIBERS; ++i) {
-        fiber_join(fibers[i]);
+        pthread_join(fibers[i], NULL);
     }
 
     test_assert(counter == NUM_FIBERS * PER_FIBER_COUNT);
