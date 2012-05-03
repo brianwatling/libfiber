e47c6ad363e72840f394cf1f36ccf42112233de2
Brian Watling <brianwatling@hotmail.com>
2012-05-03 14:45:53 -0400

    add a multi producer multi consumer lifo

---
+++ include/mpmc_lifo.h
@@ -0,0 +1,72 @@
+#ifndef _MPMC_LIFO_H_
+#define _MPMC_LIFO_H_
+
+#include "machine_specific.h"
+#include "mpsc_fifo.h"
+#include <assert.h>
+
+typedef mpsc_fifo_node_t mpmc_lifo_node_t;
+
+typedef union
+{
+    struct {
+        mpmc_lifo_node_t* volatile head;
+        uintptr_t volatile counter;
+    } data;
+    pointer_pair_t blob;
+} __attribute__ ((__packed__)) mpmc_lifo_t;
+
+static inline void mpmc_lifo_init(mpmc_lifo_t* lifo)
+{
+    assert(lifo);
+    assert(sizeof(*lifo) == sizeof(pointer_pair_t));
+    lifo->data.head = NULL;
+    lifo->data.counter = 0;
+}
+
+static inline void mpmc_lifo_destroy(mpmc_lifo_t* lifo)
+{
+    if(lifo) {
+        while(lifo->data.head) {
+            mpmc_lifo_node_t* const old = lifo->data.head;
+            lifo->data.head = lifo->data.head->next;
+            free(old);
+        }
+    }
+}
+
+static inline void mpmc_lifo_push(mpmc_lifo_t* lifo, mpmc_lifo_node_t* node)
+{
+    assert(lifo);
+    assert(node);
+    while(1) {
+        const mpmc_lifo_t snapshot = *lifo;
+        node->next = snapshot.data.head;
+        mpmc_lifo_t temp;
+        temp.data.head = node;
+        temp.data.counter = snapshot.data.counter + 1;
+        if(compare_and_swap2(&lifo->blob, &snapshot.blob, &temp.blob)) {
+            return;
+        }
+    }
+}
+
+static inline mpmc_lifo_node_t* mpmc_lifo_pop(mpmc_lifo_t* lifo)
+{
+    assert(lifo);
+    while(1) {
+        const mpmc_lifo_t snapshot = *lifo;
+        if(!snapshot.data.head) {
+            return NULL;
+        }
+        mpmc_lifo_t temp;
+        temp.data.head = snapshot.data.head->next;
+        temp.data.counter = snapshot.data.counter + 1;
+        if(compare_and_swap2(&lifo->blob, &snapshot.blob, &temp.blob)) {
+            return snapshot.data.head;
+        }
+    }
+}
+
+#endif
+
---
+++ test/test_mpmc_lifo.c
@@ -0,0 +1,83 @@
+#include <mpmc_lifo.h>
+#include "test_helper.h"
+#include <pthread.h>
+#include <stdint.h>
+#include <unistd.h>
+
+#define NUM_THREADS 4
+#define PER_THREAD_COUNT 2500000
+
+int pushes[NUM_THREADS] = {};
+int pops[NUM_THREADS] = {};
+int volatile done[NUM_THREADS] = {};
+pthread_barrier_t barrier;
+
+mpmc_lifo_t the_q;
+
+void* push_func(void* p)
+{
+    pthread_barrier_wait(&barrier);
+    intptr_t thr = (intptr_t)p;
+    intptr_t i = 0;
+    while(!done[thr]) {
+        ++i;
+        mpmc_lifo_node_t* n = malloc(sizeof(mpmc_lifo_node_t));
+        n->data = (void*)i;
+        mpmc_lifo_push(&the_q, n);
+    }
+    return NULL;
+}
+
+void* pop_func(void* p)
+{
+    pthread_barrier_wait(&barrier);
+    intptr_t thr = (intptr_t)p;
+    intptr_t last = 0;
+    (void)last;
+    intptr_t counter = 0;
+    while(!done[thr]) {
+        mpmc_lifo_node_t* node = mpmc_lifo_pop(&the_q);
+        if(!node) {
+            usleep(1);
+        } else {
+            intptr_t i = (intptr_t)node->data;
+            if(NUM_THREADS == 1) {
+                assert(i > last);
+                last = i;
+            }
+            ++counter;
+            if(counter > PER_THREAD_COUNT) {
+                done[thr] = 1;
+            }
+            free(node);
+        }
+    }
+    return NULL;
+}
+
+int main()
+{
+    pthread_barrier_init(&barrier, NULL, 2 * NUM_THREADS);
+    mpmc_lifo_init(&the_q);
+
+    pthread_t reader[NUM_THREADS];
+    pthread_t writer[NUM_THREADS];
+    int i;
+    for(i = 0; i < NUM_THREADS; ++i) {
+        pthread_create(&reader[i], NULL, &pop_func, (void*)(intptr_t)i);
+    }
+    for(i = 1; i < NUM_THREADS; ++i) {
+        pthread_create(&writer[i], NULL, &push_func, (void*)(intptr_t)i);
+    }
+
+    push_func(0);
+
+    for(i = 1; i < NUM_THREADS; ++i) {
+        pthread_join(reader[i], NULL);
+    }
+    for(i = 1; i < NUM_THREADS; ++i) {
+        pthread_join(writer[i], NULL);
+    }
+
+    return 0;
+}
