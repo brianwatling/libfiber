e7670e799baf09b492295c6156e7cf6eebc2bd4f
Brian Watling <brianwatling@hotmail.com>
2012-07-28 22:14:52 -0700

    add quick benchmark/test for go channels

---
+++ go/test_channel.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+import "time"
+
+func receiver(ch chan int) {
+    last := time.Now()
+    for i := 0; i < 100000000; i++ {
+        n := <- ch
+        if (n % 10000000) == 0 {
+            now := time.Now()
+            fmt.Printf("Received 10000000 in %g seconds\n", 0.000000001 * float64(now.Sub(last).Nanoseconds()))
+            last = now
+        }
+    }
+}
+
+func sender(ch chan int) {
+    n := 1
+    for i := 0; i < 100000000; i++ {
+        ch <- n
+        n += 1
+    } 
+}
+
+func main() {
+    ch1 := make(chan int, 1000)
+    ch2 := make(chan int, 1000)
+    go receiver(ch1)
+    go receiver(ch2)
+    go sender(ch2)
+    sender(ch1)
+}
