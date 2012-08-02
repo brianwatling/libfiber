eb23fa8954bc574918ec80d915009f2b2c97cf88
Brian Watling <brianwatling@hotmail.com>
2012-08-01 21:44:14 -0700

    update lock free perf test

--- test/test_lockfree_ring_buffer2.c
+++ test/test_lockfree_ring_buffer2.c
@@ -2,8 +2,12 @@
 #include "test_helper.h"
 #include <pthread.h>
 
-#define PER_THREAD_COUNT 10000000
-#define NUM_THREADS 4
+int64_t time_diff(const struct timespec* start, const struct timespec* end) {
+    return (end->tv_sec * 1000000000LL + end->tv_nsec) - (start->tv_sec * 1000000000LL + start->tv_nsec);
+}
+
+#define PER_THREAD_COUNT 30000000
+#define NUM_THREADS 2
 
 lockfree_ring_buffer_t* rb;
 pthread_barrier_t barrier;
@@ -21,22 +25,30 @@ void* write_function(void* param)
 void* read_function(void* param)
 {
     pthread_barrier_wait(&barrier);
+    struct timespec last;
+    clock_gettime(CLOCK_MONOTONIC, &last);
     intptr_t i;
     for(i = 1; i <= PER_THREAD_COUNT; ++i) {
         lockfree_ring_buffer_pop(rb);
+        if(i % 10000000 == 0) {
+            struct timespec now;
+            clock_gettime(CLOCK_MONOTONIC, &now);
+            printf("%p Received 10000000 in %lf seconds\n", param, 0.000000001 * time_diff(&last, &now));
+            last = now;
+        }
     }
     return NULL;
 }
 
 int main()
 {
-    rb = lockfree_ring_buffer_create(7);
+    rb = lockfree_ring_buffer_create(12);
     pthread_barrier_init(&barrier, NULL, NUM_THREADS * 2);
 
     pthread_t threads[NUM_THREADS * 2];
-    int i;
+    intptr_t i;
     for(i = 0; i < NUM_THREADS; ++i) {
-        pthread_create(&threads[i * 2], NULL, &read_function, NULL);
+        pthread_create(&threads[i * 2], NULL, &read_function, (void*)i + 1);
         pthread_create(&threads[i * 2 + 1], NULL, &write_function, NULL);
     }
 
