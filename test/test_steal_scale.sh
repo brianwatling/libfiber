b47031d5b820432954eb445330c72f98359ab1ac
Brian Watling <brianwatling@hotmail.com>
2012-09-25 00:04:44 -0700

    correction to scheduler scaling test

--- test/test_steal_scale.sh
+++ test/test_steal_scale.sh
@@ -4,14 +4,14 @@ WSDFILE=wsd_scale_data.txt
 DISTFILE=dist_scale_data.txt
 rm -f $WSDFILE $DISTFILE
 
-for THREADS in `seq 1 8`
+for THREADS in `seq 1 4`
 do
     WORK=0
     while [ $WORK -lt 250 ]
     do
         echo "$THREADS threads with $WORK work"
-        ./bin/test_wsd_scale $THREADS 10000000 $WORK | awk '/timing/{print $2, $6, $4/$8}' >> $WSDFILE
-        ./bin/test_dist_fifo $THREADS 10000000 $WORK | awk '/timing/{print $2, $6, $4/$8}' >> $DISTFILE
+        ./bin/test_wsd_scale $THREADS 10000000 $WORK | awk '/timing/{print $2, $6, ($2*$4)/$8}' >> $WSDFILE
+        ./bin/test_dist_fifo $THREADS 10000000 $WORK | awk '/timing/{print $2, $6, ($2*$4)/$8}' >> $DISTFILE
         WORK=`expr $WORK + 50`
     done
     echo >> $WSDFILE
