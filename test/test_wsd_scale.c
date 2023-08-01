8b19aafe8fe51b0a96f279e4d5c6092b58650578
Brian Watling <brian@oxbo.dev>
2023-08-01 19:17:35 -0400

    fix test_wsd_scale test

--- test/test_wsd_scale.c
+++ test/test_wsd_scale.c
@@ -150,9 +150,14 @@ int main(int argc, char* argv[]) {
 
   thread_data_t total = {};
   for (i = 0; i < NUM_THREADS; ++i) {
-    void* to_free = NULL;
-    while ((to_free = wsd_work_stealing_deque_pop_bottom(fifo[i]))) {
-      free(to_free);
+    while (1) {
+      void* to_free = wsd_work_stealing_deque_pop_bottom(fifo[i]);
+      if (to_free == WSD_EMPTY) {
+        break;
+      }
+      if (to_free != WSD_ABORT) {
+        free(to_free);
+      }
     }
     wsd_work_stealing_deque_destroy(fifo[i]);
     printf(
@@ -169,7 +174,8 @@ int main(int argc, char* argv[]) {
   free(fifo);
   free(data);
   printf(
-      "\ntotal - push: %lld pop: %lld steal: %lld attempt: %lld empty: %lld\n",
+      "\ntotal - push: %lld pop: %lld steal: %lld attempt: %lld empty: "
+      "%lld\n",
       total.push_count, total.pop_count, total.steal_count, total.attempt_count,
       total.empty_count);
 
